3f3a852791f3dd2775459c128ac8b529c4386b2e
mathe2810 <matheogleon92100@yahoo.com>
2023-04-24 02:01:22 +0200

    include fonction_allegro.h into main.c

--- main.c
+++ main.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include "allegro.h"
-
-
-///fonction allegro
-
-void initiation()
-{
-    allegro_init();
-    set_color_depth(desktop_color_depth());
-    if(set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,800,0,0)!=0)
-    {
-        allegro_message("problem gfx");
-        allegro_exit();
-        exit(EXIT_FAILURE);
-    }
-    install_keyboard();
-    install_mouse();
-    show_mouse(screen);
-}
-
-BITMAP * importeImage(char *nomDeFichier)
-{
-    BITMAP *imageARendre= load_bitmap(nomDeFichier,NULL);
-    if(!imageARendre)
-    {
-        allegro_message("ne peut pas ouvrir");
-        allegro_exit();
-        exit(EXIT_FAILURE);
-    }
-    return imageARendre;
-}
+#include "fonction_allegro.h"
 
 int main(void) {
     printf("Hello World!\n");
