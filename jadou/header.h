fc8e2344d7acbc52b92aff3b6723ba7f01bf9be9
jade <jade.vigou@gmail.com>
2023-05-17 22:28:14 +0200

    jeu fini mais quelques bug a regler

--- jadou/header.h
+++ jadou/header.h
@@ -1,28 +1,33 @@
 #ifndef PROJET_HEADER_H
 #define PROJET_HEADER_H
 
-#include "stdio.h"
-#include "allegro.h"
-#include "time.h"
+#include <allegro.h>
+#include <time.h>
+#include <stdio.h>
 
 #define NELEM 10
 
-typedef struct jeu_cible_s {
-    int pos_x[NELEM],pos_y[NELEM];
-    int dep_x[NELEM],dep_y[NELEM];
+typedef struct jeu_ballon_s {
+    int posx[NELEM],posy[NELEM];
+    int depx[NELEM],depy[NELEM];
+    clock_t temps_depop[NELEM];
     int score;
-    int balle_tir;
-    BITMAP *cible;
-    clock_t clock_bouge_cible;
+    int balle_tire;
+    BITMAP *ballon;
+    clock_t clock_bouge_ballon;
     SAMPLE *gagnee;
     SAMPLE *perdue;
     clock_t temps_restant;
     BITMAP *gagne_image;
     BITMAP *perdue_image;
-    SAMPLE *tir_pistolet;
-} jeu_cible_t;
+    SAMPLE *ballon_pop;
+    BITMAP *fond;
+    BITMAP *ballon2;
+} jeu_ballon_t;
 
-jeu_cible_t creer_partie(void);
-
+jeu_ballon_t creer_partie(void);
+void ballon_deplace(jeu_ballon_t *jeu, int rayon);
+void ballon_toucher(jeu_ballon_t *jeu, int i);
+void gagne_ou_perdue(jeu_ballon_t jeu);
 
 #endif //PROJET_HEADER_H
--- jadou/main.c
+++ jadou/main.c
@@ -1,9 +1,61 @@
 #include "header.h"
 
+int main()
+{
+    BITMAP *page;
+    char scoreString[5];
+    jeu_ballon_t jeu;
+    int shoot = 0;
 
-int main (){
+    srand(time(NULL));
 
+    allegro_init();
+    install_keyboard();
+    install_mouse();
+    install_sound(DIGI_AUTODETECT, MIDI_NONE, "");
+
+    set_color_depth(desktop_color_depth());
+    if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+
+    page=create_bitmap(SCREEN_W,SCREEN_H);
+    jeu = creer_partie();
+
+    show_mouse(screen);
+    while (!key[KEY_ESC])
+    {
+        clear_bitmap(page);
+        stretch_sprite(page, jeu.fond, 0, 0, SCREEN_W, SCREEN_H);
+        ballon_deplace(&jeu, jeu.ballon->w / 6);
+        for (int i = 0; i < NELEM; i++) {
+            ballon_toucher(&jeu, i);
+            if (jeu.temps_depop[i] == 0)
+                stretch_sprite(page, jeu.ballon, jeu.posx[i], jeu.posy[i], jeu.ballon->w / 3, jeu.ballon->h / 3);
+            else if (clock() - jeu.temps_depop[i] <= 150)
+                stretch_sprite(page, jeu.ballon2, jeu.posx[i], jeu.posy[i], jeu.ballon->w / 3, jeu.ballon->h / 3);
+            else
+                jeu.posx[i] = -100;
+        }
+        jeu.temps_restant = clock();
+        gagne_ou_perdue(jeu);
+
+        sprintf(scoreString, "%d", 15 - (jeu.temps_restant / 1000));
+        textout_ex(page, font, "temps restant:", SCREEN_W - 200, SCREEN_H - 30, makecol(255, 255, 255), -1);
+        textout_ex(page, font, scoreString, SCREEN_W - 30, SCREEN_H - 30, makecol(255, 255, 255), -1);
+
+        sprintf(scoreString, "%d", jeu.score);
+        textout_ex(page, font, "Score:", 30, SCREEN_H - 30, makecol(255, 255, 255), -1);
+        textout_ex(page, font, scoreString, 90, SCREEN_H - 30, makecol(255, 255, 255), -1);
+        blit(page,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+
+    }
     allegro_exit();
     return 0;
-}END_OF_MAIN();
+}
+END_OF_MAIN();
+
 
